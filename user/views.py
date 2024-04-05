fad67865df2060dbb89ecbbe5414fad53af7420a
Mayur <mayurpatkar68@gmail.com>
2024-04-05 05:37:18 +0530

    delete account

--- user/views.py
+++ user/views.py
@@ -15,6 +15,7 @@ import hashlib
 client = MongoClient('mongodb://localhost:27017/')
 db = client['intelliAi']
 collection = db['accounts']
+chat_history_collection = db['conv_history']
 
 def hash_password(password):
     # SHA-256 algorithm
@@ -48,13 +49,10 @@ def index(request):
         return render(request, 'landingPage.html')
 
 def intelliAi(request):
-    email = None
     if 'session_token' in request.COOKIES:
         session_token = request.COOKIES['session_token']
         user = collection.find_one({'session_token': session_token})
-        if user:
-            email = user.get('email')
-    return render(request, 'intelliAi.html', {'email': email})
+    return render(request, 'intelliAi.html')
 
 def signup(request):
     if request.method == 'POST':
@@ -107,6 +105,10 @@ def ai_response(request):
         prompt = request.POST.get('prompt', '')
         image = request.FILES.get('image', None)
         ai_response = responce(prompt, image)
+        timestamp = datetime.now()
+        chat_history = {'prompt':prompt, 'response':ai_response, 'timestamp':timestamp}
+        chat_history_collection.insert_one(chat_history)
+
         return JsonResponse({'message': ai_response})
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)
@@ -128,18 +130,16 @@ def logout(request):
 @csrf_exempt
 def deleteAccount(request):
     if request.method == 'POST':
-        try:
-            email = request.POST.get('email')
-            print(email)
-            if email:
-                collection.delete_one({'email': email})
-                response = JsonResponse({'message': 'success'})
-                response.delete_cookie('session_token')
-                return response
-            else:
-                return JsonResponse({'error': 'Email not provided'}, status=400)
-        except Exception as e:
-            return JsonResponse({'error': str(e)}, status=500)
+        session_token = request.COOKIES.get('session_token')
+        user = collection.find_one({'session_token': session_token})
+        if user:
+            email = user['email']
+            collection.delete_one({'email': email})
+            response = JsonResponse({'message': 'success'})
+            response.delete_cookie('session_token')
+            return response
+        else:
+            return JsonResponse({'error': 'user not found!'}, status=400)
     else:
         return JsonResponse({'error': 'Method not allowed'}, status=405)
 
